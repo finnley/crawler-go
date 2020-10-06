97be760730a1fb225450455ccfd75f8c0b8292da
finnley <finnley.yuan@jingdigital.com>
2020-10-06 10:07:02 +0800

    city parser

--- zhenai/parser/city.go
+++ zhenai/parser/city.go
@@ -5,21 +5,20 @@ import (
 	"regexp"
 )
 
-const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
 
-func ParseCityList(contents []byte) engine.ParseResult {
-	//现在只需要提取城市和url,改造如下
-	re := regexp.MustCompile(cityListRe)
+func ParseCity(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityRe)
 	matches := re.FindAllSubmatch(contents, -1)
 
 	//对于每一个url生成一个新的request
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		//fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
-		result.Items = append(result.Items, "City " + string(m[2]))
+		result.Items = append(result.Items, "User " + string(m[2]))
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
-			ParserFunc: ParseCity,
+			ParserFunc: engine.NilParser,
 		})
 	}
 
